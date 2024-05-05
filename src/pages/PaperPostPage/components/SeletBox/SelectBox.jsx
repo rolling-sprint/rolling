9c041c9c262a3079b8e7b03265795ed4fe711950
YoungUnKim <162089313+YoungUnKim@users.noreply.github.com>
2024-05-05 20:53:34 +0900

    Feat : PaperPostForm 작성
    - 토글 선택에 따른 배경&컬러 선택 화면 작성 작업
    - 폼 이벤트 및 버튼 보이기/안보이기 에러 이벤트 작업

--- src/pages/PaperPostPage/components/SeletBox/SelectBox.jsx
+++ src/pages/PaperPostPage/components/SeletBox/SelectBox.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
-import ToggleButton from "../components/ToggleButton/ToggleButton";
+import { getImages } from "../../../../services/api";
+import ToggleButton from "../ToggleButton/ToggleButton";
+import Checkbox from "../CheckBox/CheckBox";
+import checkImage from "../../../../assets/images/Enabled.svg";
 import styles from "./SelectBox.module.scss";
 
 const Select = [
@@ -7,8 +10,62 @@ const Select = [
   { label: "이미지", value: "image" },
 ];
 
-function SelectBox({ onSelectTypeChange }) {
+const SelectColorDefault = {
+  ckb1: true,
+  ckb2: false,
+  ckb3: false,
+  ckb4: false,
+};
+
+function SelectBox({ onSelectionChange, onSelectTypeChange }) {
   const [type, setType] = useState("color");
+  const [isChecked, setIsChecked] = useState(SelectColorDefault);
+  const [imageUrls, setImageUrls] = useState([]);
+
+  const handleCheckboxChange = (checkboxId) => {
+    const newIsChecked = {
+      ckb1: false,
+      ckb2: false,
+      ckb3: false,
+      ckb4: false,
+    };
+
+    newIsChecked[checkboxId] = true;
+
+    setIsChecked(newIsChecked);
+
+    const index = parseInt(checkboxId.slice(3)) - 1;
+
+    const newBackgroundColor = getColorFromCheckbox(checkboxId) || "";
+    const newImageURL = imageUrls[index] || "";
+
+    onSelectionChange({
+      backgroundColor: newBackgroundColor,
+      imageURL: newImageURL,
+    });
+  };
+
+  const getColorFromCheckbox = (checkboxId) => {
+    switch (checkboxId) {
+      case "ckb1":
+        return "beige";
+      case "ckb2":
+        return "purple";
+      case "ckb3":
+        return "blue";
+      case "ckb4":
+        return "green";
+      default:
+        return "beige";
+    }
+  };
+
+  useEffect(() => {
+    const fetchImageUrls = getImages("background-images");
+    if (fetchImageUrls && fetchImageUrls.length > 0) {
+      setImageUrls(fetchImageUrls);
+    }
+  }, []);
 
   return (
     <>
@@ -21,6 +78,21 @@ function SelectBox({ onSelectTypeChange }) {
             onSelectTypeChange(type);
           }}
         />
+      </div>
+
+      <div className={styles.selectorBox}>
+        {imageUrls.map((imageUrl, index) => (
+          <Checkbox
+            key={`ckb${index + 1}`}
+            id={`ckb${index + 1}`}
+            type={type}
+            color={getColorFromCheckbox(`ckb${index + 1}`)}
+            image={`image${index + 1}`}
+            isChecked={isChecked[`ckb${index + 1}`]}
+            onCheckboxChange={() => handleCheckboxChange(`ckb${index + 1}`)}
+            checkIcon={<image src={checkImage} className={styles.check} />}
+          />
+        ))}
       </div>
     </>
   );
