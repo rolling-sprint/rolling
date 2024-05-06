7c55fc5ee9cfa518c5859f87da2e03b585766250
yeonilil <gwagseoyeon@gmail.com>
2024-05-06 19:44:47 +0900

    Feat:formatDate함수 추가

--- src/pages/MyPaperPage/components/MyPaperCard.jsx
+++ src/pages/MyPaperPage/components/MyPaperCard.jsx
@@ -1,9 +1,11 @@
 import styles from "./MyPaperCard.module.scss";
 
-// function formatDate(value) {
-//   const date = new Date(value);
-//   return `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()}`;
-// }
+function formatDate(value) {
+  const date = new Date(value);
+  return `${date.getFullYear()}.${
+    date.getMonth() + 1 > 9 ? date.getMonth() + 1 : "0" + (date.getMonth() + 1)
+  }.${date.getDate() + 1 > 9 ? date.getDate() : "0" + date.getDate()}`;
+}
 
 function MyPaperCard({ message }) {
   console.log(message);
@@ -11,13 +13,13 @@ function MyPaperCard({ message }) {
     <div className="page-wrapper">
       <div className={styles.messageCard}>
         <div className={styles.senderHeader}>
-          <img src={message.profileImageURL} />
+          <img src={message.profileImageURL} alt="프로필 이미지" />
           <p className={styles.sender}>From. {message.sender}</p>
           <p className={styles.relationship}>{message.relationship}</p>
         </div>
         <div className={styles.messageBorderLine}></div>
         <p className={styles.content}>{message.content}</p>
-        <p className={styles.createDate}>{message.createdAt}</p>
+        <p className={styles.createDate}>{formatDate(message.createdAt)}</p>
       </div>
     </div>
   );
