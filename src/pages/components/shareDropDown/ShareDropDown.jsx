89863e89530600170744327c4f307db781276c8a
yongin <rladls6412@naver.com>
2024-05-08 00:31:07 +0900

    Feat: 토스트기능 추가 및 CSS 적용

--- src/pages/components/shareDropDown/ShareDropDown.jsx
+++ src/pages/components/shareDropDown/ShareDropDown.jsx
@@ -1,28 +1,64 @@
 import { useState } from "react";
 import styles from "./ShareDropDown.module.scss";
 import shareImg from "./imgs/share.svg";
+import completedImg from "./imgs/completed.svg";
+import closeImg from "./imgs/close.svg";
 
 const ShareDropDown = () => {
   const [shareShow, setShareShow] = useState(false);
+  const [toast, setToast] = useState(false);
 
   const onClickShow = () => {
     setShareShow(!shareShow);
   };
 
+  const onHandleCopyURL = () => {
+    navigator.clipboard.writeText(window.location.href);
+    setToast(true);
+
+    setTimeout(() => {
+      setToast(false);
+    }, [5000]);
+  };
+
+  const onCloseBtn = () => {
+    setToast(false);
+  };
+
   return (
-    <div className={styles.share_container}>
-      <button className={styles.share_btn} onClick={onClickShow}>
-        <img src={shareImg} alt="공유이미지" />
-      </button>
-      <div className={styles.outside_container}>
-        {shareShow && (
-          <div className={styles.outside_share}>
-            <div className={styles.share_list}>카카오톡 공유</div>
-            <div className={styles.share_list}>URL 공유</div>
-          </div>
-        )}
+    <>
+      <div className={styles.share_container}>
+        <button className={styles.share_btn} onClick={onClickShow}>
+          <img src={shareImg} alt="공유이미지" />
+        </button>
+        <div className={styles.outside_container}>
+          {shareShow && (
+            <div className={styles.outside_share}>
+              <div className={styles.share_list}>카카오톡 공유</div>
+              <div onClick={onHandleCopyURL} className={styles.share_list}>
+                URL 공유
+              </div>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+      {toast && (
+        <div className={styles.toast_box}>
+          <div className={styles.toast_message}>
+            <img src={completedImg} alt="완료이미지" />
+            URL 카피완료
+          </div>
+          <div>
+            <img
+              className={styles.closebtn}
+              onClick={onCloseBtn}
+              src={closeImg}
+              alt="닫기이미지"
+            />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 export default ShareDropDown;
