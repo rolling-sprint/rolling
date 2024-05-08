48339805bd9e25d6e6a4fa73de7a46a39b1f11fe
Jiyun Kim <wayandway@kakao.com>
2024-05-08 15:46:49 +0900

    Fix: 공유드롭다운 import 테스팅 코드 삭제

--- src/pages/MyPaperPage/MyPaperPage.jsx
+++ src/pages/MyPaperPage/MyPaperPage.jsx
@@ -1,12 +1,5 @@
-import ShareDropDown from "../components/shareDropDown/ShareDropDown";
-
 function MyPaperPage() {
-  return (
-    <div className="page-wrapper">
-      MyPaperPage
-      <ShareDropDown />
-    </div>
-  );
+  return <div className="page-wrapper">MyPaperPage</div>;
 }
 
 export default MyPaperPage;
