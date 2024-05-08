73d3bc4ad10af6827635916b1d52dab458a082d6
임진조 <dlawlswh123@gmail.com>
2024-05-09 00:20:54 +0900

    Style: 모바일에서 padding 추가, 글자사이즈 변경

--- src/pages/PaperListPage/PaperListPage.jsx
+++ src/pages/PaperListPage/PaperListPage.jsx
@@ -4,31 +4,23 @@ import PrimaryButton from "../../components/UI/PrimaryButton";
 import { useNavigate } from "react-router-dom";
 function PaperListPage() {
   const navigate = useNavigate();
-  const style = {
-    justifyContent: "center",
-    alignItems: "center",
-    padding: "124px 0",
-    margin: "0 auto",
-  };
 
   const handlePage = () => {
     navigate("/post");
   };
 
   return (
-    <div className="page-wrapper" style={style}>
-      <div className={styles.container}>
-        <div className={styles.hot}>
-          <h1>인기 롤링 페이퍼</h1>
-          <CardList order="like" />
-        </div>
-        <div className={styles.new}>
-          <h1>최근에 만든 롤링 페이퍼</h1>
-          <CardList />
-        </div>
-        <div className={styles.buttonContainer}>
-          <PrimaryButton onClick={handlePage}>나도 만들어 보기</PrimaryButton>
-        </div>
+    <div className={styles.container}>
+      <div className={styles.hot}>
+        <h1>인기 롤링 페이퍼 🔥</h1>
+        <CardList order="like" />
+      </div>
+      <div className={styles.new}>
+        <h1>최근에 만든 롤링 페이퍼⭐</h1>
+        <CardList />
+      </div>
+      <div className={styles.buttonContainer}>
+        <PrimaryButton onClick={handlePage}>나도 만들어 보기</PrimaryButton>
       </div>
     </div>
   );
