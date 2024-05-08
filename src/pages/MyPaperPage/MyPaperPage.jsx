6eb1ee6f88b7232cdf0f0a759cf2f418d0fd5e5c
yongin <rladls6412@naver.com>
2024-05-08 15:26:10 +0900

    이미지위치수정 및 카카오톡 공유 로고 변경

--- src/pages/MyPaperPage/MyPaperPage.jsx
+++ src/pages/MyPaperPage/MyPaperPage.jsx
@@ -1,5 +1,12 @@
+import ShareDropDown from "../components/shareDropDown/ShareDropDown";
+
 function MyPaperPage() {
-  return <div className="page-wrapper">MyPaperPage</div>;
+  return (
+    <div className="page-wrapper">
+      MyPaperPage
+      <ShareDropDown />
+    </div>
+  );
 }
 
 export default MyPaperPage;
--- src/pages/components/shareDropDown/ShareDropDown.jsx
+++ src/pages/components/shareDropDown/ShareDropDown.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import styles from "./ShareDropDown.module.scss";
-import shareImg from "./imgs/share.svg";
-import completedImg from "./imgs/completed.svg";
-import closeImg from "./imgs/close.svg";
+import shareImg from "../../../assets/images/share.svg";
+import completedImg from "../../../assets/images/completed.svg";
+import closeImg from "../../../assets/images/close.svg";
 
 // 카톡 공유하기 양식
 const kakaoButton = () => {
@@ -19,7 +19,7 @@ const kakaoButton = () => {
         title: "롤링 프로젝트",
         description: "#코드잇 #10팀 #리액트 #살려줘",
         imageUrl:
-          "https://miro.medium.com/v2/resize:fit:1200/0*XCgoYU9sqt95P8J0.png",
+          "https://github-production-user-asset-6210df.s3.amazonaws.com/64190056/328775094-19d8b7d8-0429-40b8-ae41-aa02fce49903.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20240508%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240508T061553Z&X-Amz-Expires=300&X-Amz-Signature=0d0de67b30e1a7c16b48600340d35f951a39f9f9b8a4bca4ac76a914b2ae1b83&X-Amz-SignedHeaders=host&actor_id=105802492&key_id=0&repo_id=793432320",
         link: {
           mobileWebUrl: "https://developers.kakao.com",
           webUrl: "https://developers.kakao.com",
@@ -90,7 +90,7 @@ const ShareDropDown = () => {
         <div className={styles.toast_box}>
           <div className={styles.toast_message}>
             <img src={completedImg} alt="완료이미지" />
-            URL 카피완료
+            URL이 복사 되었습니다.
           </div>
           <div>
             <img
