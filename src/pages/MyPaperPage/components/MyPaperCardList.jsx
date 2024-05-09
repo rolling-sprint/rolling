e5deff4e063a2b60b62a56d34d0beab05f3b74a4
yeonilil <gwagseoyeon@gmail.com>
2024-05-09 10:50:58 +0900

    Fix: 배경 이미지, 배경색 API 연결

--- src/pages/MyPaperPage/components/MyPaperCardList.jsx
+++ src/pages/MyPaperPage/components/MyPaperCardList.jsx
@@ -1,7 +1,7 @@
 import { MyPaperCard } from "./MyPaperCard";
 import styles from "./MyPaperCardList.module.scss";
 import { useEffect, useState } from "react";
-import { getMessages } from "../../../services/api";
+import { getMessages, getRecipientDetail } from "../../../services/api";
 import AddPaperCard from "./AddPaperCard";
 import Modal from "./Modal/Modal";
 
@@ -14,8 +14,13 @@ function MyPaperCardList({ id }) {
     font: "",
     createdAt: "",
   };
+  const INITIAL_RECIPIENT_VALUE = {
+    backgroundColor: "",
+    backgroundImageURL: null,
+  };
 
   const [userMessage, setUserMessage] = useState(INITIAL_VALUE);
+  const [recipient, setRecipient] = useState(INITIAL_RECIPIENT_VALUE);
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [selectedMessage, setSelectedMessage] = useState(null);
   const onClickButton = (message) => {
@@ -26,11 +31,38 @@ function MyPaperCardList({ id }) {
   const handleLoad = async () => {
     const message = await getMessages(id);
     setUserMessage(message);
+    const user = await getRecipientDetail(id);
+    setRecipient(user);
   };
 
   useEffect(() => {
     handleLoad();
   }, []);
+
+  const ColorNumber = (color) => {
+    switch (color) {
+      case "beige":
+        return "#ffe2ad";
+      case "purple":
+        return "#ecd9ff";
+      case "blue":
+        return "#b1e4ff";
+      case "green":
+        return "#d0f5c3";
+      default:
+        return "#ffe2ad";
+    }
+  };
+
+  const ColorStyle = {
+    background: `${ColorNumber(recipient.backgroundColor)}`,
+  };
+
+  const imageStyle = {
+    backgroundImage: recipient.backgroundImageURL
+      ? `url(${recipient.backgroundImageURL})`
+      : "none",
+  };
 
   const sortedItems = userMessage.results
     ? userMessage.results
@@ -39,7 +71,10 @@ function MyPaperCardList({ id }) {
     : [];
 
   return (
-    <div className={styles.background}>
+    <div
+      className={styles.background}
+      style={recipient.backgroundImageURL ? imageStyle : ColorStyle}
+    >
       <div className={styles.cardList}>
         <AddPaperCard className={styles.addCard} />
         {sortedItems &&
