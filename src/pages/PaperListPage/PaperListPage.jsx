f608ecdc0622bb6db8d1d0f68c1d321403151c92
jinjo <dlawlswh123@gmail.cllm>
2024-05-11 19:12:33 +0900

    Refactor: 함수, 변수명 변경

--- src/pages/PaperListPage/PaperListPage.jsx
+++ src/pages/PaperListPage/PaperListPage.jsx
@@ -2,16 +2,23 @@ import CardList from "./components/cardList/CardList";
 import styles from "./PaperListPage.module.scss";
 import PrimaryButton from "../../components/UI/PrimaryButton";
 import { useNavigate } from "react-router-dom";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 function PaperListPage() {
   const navigate = useNavigate();
-  const widthMax = useRef(false);
-  const [isMobile, setIsMobile] = useState(null);
-  const [isPhone, setIsPhone] = useState(null);
   const [windowSize, setWindowSize] = useState({
     width: window.innerWidth,
     height: window.innerHeight,
   });
+
+  const isMobile = windowSize.width < 1920;
+  const isPhone = windowSize.width> 360 && windowSize.width < 768;
+
+  const handleResize = () => {
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  };
 
   useEffect(() => {
     handleResize();
@@ -21,42 +28,23 @@ function PaperListPage() {
     };
   }, []);
 
-  useEffect(() => {
-    if (windowSize.width >= 360 && windowSize.width < 768) {
-      widthMax.current = true;
-    } else {
-      widthMax.current = false;
-    }
-  }, [windowSize]);
 
-  const handleResize = () => {
-    setWindowSize({
-      width: window.innerWidth,
-      height: window.innerHeight,
-    });
+  const handleNavigationToPostPage = (cardId) => {
+    navigate(`/post/${cardId}`);
   };
-
-  const handlePage = () => {
-    navigate("/post");
-  };
-
-  useEffect(() => {
-    setIsMobile(windowSize.width < 1920);
-    setIsPhone(windowSize.width >= 360 && windowSize.width < 768);
-  }, [windowSize]);
 
   return (
     <div className={styles.container}>
       <div className={styles.hot}>
-        <h1>인기 롤링 페이퍼 🔥</h1>
-        <CardList order="like" isMobile={isMobile} isPhone={isPhone} />
+        <h1 className={styles.h1}>인기 롤링 페이퍼 🔥</h1>
+        <CardList order="like" isMobile={isMobile} isPhone={isPhone} onClick={handleNavigationToPostPage}/>
       </div>
       <div className={styles.new}>
-        <h1>최근에 만든 롤링 페이퍼⭐</h1>
-        <CardList isMobile={isMobile} isPhone={isPhone} />
+        <h1 className={styles.h1}>최근에 만든 롤링 페이퍼⭐</h1>
+        <CardList isMobile={isMobile} isPhone={isPhone} onClick={handleNavigationToPostPage}/>
       </div>
       <div className={styles.buttonContainer}>
-        <PrimaryButton onClick={handlePage} WidthMax={widthMax.current}>
+        <PrimaryButton onClick={() =>handleNavigationToPostPage("")} WidthMax={isMobile}>
           나도 만들어 보기
         </PrimaryButton>
       </div>
