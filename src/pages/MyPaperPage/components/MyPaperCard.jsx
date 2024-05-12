86f9c4fa2e4b29a3588710117a39c7f1e5e950ec
Jiyun Kim <wayandway@kakao.com>
2024-05-12 20:56:21 +0900

    Feat: 메시지에 폰트 적용 & relationship() 함수 수정

--- src/pages/MyPaperPage/components/MyPaperCard.jsx
+++ src/pages/MyPaperPage/components/MyPaperCard.jsx
@@ -6,11 +6,31 @@ export function formatDate(value) {
     date.getMonth() + 1 > 9 ? date.getMonth() + 1 : "0" + (date.getMonth() + 1)
   }.${date.getDate() + 1 > 9 ? date.getDate() : "0" + date.getDate()}`;
 }
-function relationship(message) {
-  if (message.relationship === "친구") return styles.blue;
-  else if (message.relationship === "지인") return styles.orange;
-  else if (message.relationship === "가족") return styles.green;
-  else if (message.relationship === "동료") return styles.purple;
+
+function setRelationship(relationship) {
+  switch (relationship) {
+    case "친구":
+      return styles.blue;
+    case "지인":
+      return styles.orange;
+    case "가족":
+      return styles.green;
+    case "동료":
+      return styles.purple;
+  }
+}
+
+function setFont(font) {
+  switch (font) {
+    case "Pretendard":
+      return "Pretendard";
+    case "Noto Sans":
+      return "Noto Sans";
+    case "나눔명조":
+      return "NanumMyeongjo";
+    case "나눔손글씨 손편지체":
+      return "NanumPen";
+  }
 }
 
 export function MyPaperCard({ message, onClick }) {
@@ -20,12 +40,21 @@ export function MyPaperCard({ message, onClick }) {
         <img src={message.profileImageURL} alt="프로필 이미지" />
         <p className={styles.sender}>From.</p>
         <p className={styles.senderName}>{message.sender}</p>
-        <p className={`${styles.relationship} ${relationship(message)}`}>
+        <p
+          className={`${styles.relationship} ${setRelationship(
+            message.relationship
+          )}`}
+        >
           {message.relationship}
         </p>
       </div>
-      <hr />
-      <p className={styles.content}>{message.content}</p>
+      <hr className={styles.hr} />
+      <p
+        className={styles.content}
+        style={{ fontFamily: setFont(message.font) }}
+      >
+        {message.content}
+      </p>
       <p className={styles.createDate}>{formatDate(message.createdAt)}</p>
     </div>
   );
