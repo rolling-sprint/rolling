d507f76e976df709df343a362ea6a9d718ad10ef
yongin <rladls6412@naver.com>
2024-05-13 13:50:33 +0900

    모바일에서 공유하기헤더 보이도록 스타일 수정완료

--- src/pages/components/emojiAdd/EmojiAdd.jsx
+++ src/pages/components/emojiAdd/EmojiAdd.jsx
@@ -1,4 +1,3 @@
-import { postReaction } from "../../../services/api";
 import styles from "./EmojiAdd.module.scss";
 import addImg from "../../../assets/images/add.svg";
 import EmojiPicker from "emoji-picker-react";
@@ -7,6 +6,7 @@ import { useEffect, useState } from "react";
 const EmojiAdd = ({ handlePostEmojiData }) => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [emojiShow, setEmojiShow] = useState(false);
+  const style = windowWidth <= 500 ? 250 : 350;
 
   const handleEmojiShow = () => {
     setEmojiShow(!emojiShow);
@@ -41,7 +41,13 @@ const EmojiAdd = ({ handlePostEmojiData }) => {
         )}
       </button>
       <div className={styles.emoji_picker_container}>
-        {emojiShow && <EmojiPicker onEmojiClick={onEmojiAddClick} />}
+        {emojiShow && (
+          <EmojiPicker
+            // 이모지라이브러리 반응형 사이즈조절하기위해서 인라인스타일지정 (라이브러리에서 인라인으로 스타일이 적용되어있어서 scss 적용안됨)
+            style={{ width: style }}
+            onEmojiClick={onEmojiAddClick}
+          />
+        )}
       </div>
     </div>
   );
